669481d4cd7bcd19687da8351e9715ce75b5126a
Brian Mwangi <momanyibrian18@gmail.com>
2023-03-14 20:36:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const afterEach = () =>{
+    return (drivers, 'MAKE SURE YOUR ARRAY MANIPULATIONS ARE NON-DESTRUCTIVE').to.eql(['Antonia', 'Nuru', 'Amari', 'Mo'])
+}
+function returnFirstTwoDrivers(){
+    return drivers.slice(0, 2);
+}
+function returnLastTwoDrivers(){
+    return drivers.slice(-2);
+}
+// selectingDrivers array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier function
+function createFareMultiplier(integer) {
+  return function(value) {
+    return integer * value;
+  }
+}
+
+// fareDoubler function
+function fareDoubler(fare) {
+  return createFareMultiplier(2)(fare);
+}
+
+// fareTripler function
+function fareTripler(fare) {
+  return createFareMultiplier(3)(fare);
+}
+
+// selectDifferentDrivers function
+function selectDifferentDrivers(drivers, fn) {
+  return fn(drivers);
+}
+
+
+
+
